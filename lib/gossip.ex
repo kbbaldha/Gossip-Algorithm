cedd581c9ab60d2c7945750d58a34ddfc43fbc20
kbbaldha <keyurbaldha@gmail.com>
2017-09-30 20:35:27 -0400

    Add imperfect 2D topology

--- lib/gossip.ex
+++ lib/gossip.ex
@@ -34,16 +34,23 @@ defmodule MAIN do
   
   def get_line_neighbours(total_nodes,cur_node_id) do
     lst = []
-    if(cur_node_id == 1) do
-      lst = [cur_node_id + 1|lst]
-    end
-    else if(cur_node_id == total_nodes) 
-      lst = [cur_node_id - 1| lst]
+    # if(cur_node_id == 1) do
+    #   lst = [cur_node_id + 1|lst]
     
-    else 
-      lst = [cur_node_id+1|lst]
-      lst = [cur_node_id-1|lst]
+    # else if(cur_node_id == total_nodes) do
+    #   lst = [cur_node_id - 1| lst]
+    
+    # else do
+    #   lst = [cur_node_id+1|lst]
+    #   lst = [cur_node_id-1|lst]
+    # end
+
+    cond do
+      cur_node_id == 1 -> lst = [cur_node_id + 1|lst]
+      cur_node_id == 1 -> lst = [cur_node_id + 1|lst]
+      true -> lst = [cur_node_id + 1|lst]
     end
+
     lst
   end
   
@@ -62,7 +69,7 @@ defmodule MAIN do
 
   ## 2D grid
 
-  
+
   def get_2D_neighbours(dimension,cur_node_id) do
     column_number = round :math.fmod cur_node_id,dimension
     top = cur_node_id - dimension
@@ -71,17 +78,25 @@ defmodule MAIN do
       lst = [top|lst]
     end 
     down = cur_node_id + dimension
-    if(down < dimension*dimension)do
-      lst = lst[down|lst]
+    if(down < dimension*dimension) do
+      lst = [down|lst]
     end
 
-    ##side neighbours
-    if(column_number == 0) do ## last column
-       lst = [cur_node_id - 1|lst]
-    else if(column_number == 1)
-       lst = [cur_node_id+1|lst]
-    else 
-      lst = [cur_node_id - 1|lst]
+    # ##side neighbours
+    # if(column_number == 0) do ## last column
+    #    lst = [cur_node_id - 1|lst]
+    
+    # else if(column_number == 1)  
+    #    lst = [cur_node_id+1|lst]
+    
+    # else 
+    #   lst = [cur_node_id - 1|lst]
+    #   lst = [cur_node_id + 1|lst]
+    # end
+    cond do
+      column_number == 0 -> lst = [cur_node_id - 1|lst]
+      column_number == 1 -> lst = [cur_node_id+1|lst]
+      true -> lst = [cur_node_id - 1|lst]
       lst = [cur_node_id + 1|lst]
     end
     lst
@@ -90,15 +105,44 @@ defmodule MAIN do
   def build_2D_topology(state) do
     total_nodes = Map.get(state,"total_nodes")
     dimension = round :math.ceil :math.sqrt(total_nodes)
-    Math.put(state,"total_nodes",dimension*dimension)
+    total_nodes = dimension*dimension
+    Map.put(state,"total_nodes",total_nodes)
+    IO.puts "total_nodes :::::" <> Integer.to_string total_nodes
     cur_node_id = Map.get(state,"id")
-    neighbours = get_2D_neighbours(total_nodes,cur_node_id)
+    neighbours = get_2D_neighbours(dimension,cur_node_id)
+    state = Map.put(state,"neighbours",neighbours)
+    IO.inspect state
+    state
+  end
+
+  ##2D grid ends
+
+  ##imperfect 2D start
+
+  def get_imperfect2D_neighbours(total_nodes,dimension,cur_node_id) do
+    
+    lst = get_2D_neighbours(dimension,cur_node_id)
+    #add random node
+    lst = [:rand.uniform(total_nodes)|lst]
+    lst
+  end
+
+  def build_imperfect2D_topology(state) do
+    total_nodes = Map.get(state,"total_nodes")
+    dimension = round :math.ceil :math.sqrt(total_nodes)
+    total_nodes = dimension*dimension
+    Map.put(state,"total_nodes",total_nodes)
+
+    cur_node_id = Map.get(state,"id")
+    neighbours = get_imperfect2D_neighbours(total_nodes,dimension,cur_node_id)
     state = Map.put(state,"neighbours",neighbours)
     #IO.inspect state
     state
   end
 
-  ##2D grid ends
+  ##imperfect 2d end
+
+
   def start_nodes(n,total) when n<1 do
     IO.puts "all nodes created"
     send_message("node_"<>Integer.to_string(10))
@@ -118,7 +162,8 @@ defmodule MAIN do
     #node_name = "node_" <> Integer.to_string(elem(args,0))
     node_id = elem(args,0)
     map =  %{"id" => node_id,"total_nodes" => total_nodes, "neighbours" => []}    
-    state = build_mesh_topology(map)
+    #state = build_mesh_topology(map)
+    state = build_2D_topology(map)
     {:ok,state}
   end
 
